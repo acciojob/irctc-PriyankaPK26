de3844f6a55e4a380d8ba7adc4615a9b95d0bffb
Priyanka Prashant Khirapate <priyankakhirapate@gmai.com>
2023-12-03 16:40:28 +0530

    4th attemt

--- src/main/java/com/driver/Irctc.java
+++ src/main/java/com/driver/Irctc.java
@@ -7,7 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-public claIrctc {
+public class Irctc {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Irctc.class, args);
